db8757d759cd2d09e4f90f115328771653bff963
jiwon821 <251018584+jiwon821@users.noreply.github.com>
2026-06-03 10:12:57 +0300

    Move lookback buffer to its own files

---
+++ src/lookback_buffer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "stdafx.h"
+
+#include <mutex>
+
+// used as a buffer for rewinding, dunno much more than that yet
+class lookback_buffer
+{
+public:
+    lookback_buffer() : max_size_(0), buf_(), out_buf_(), lookback_(0)
+    {
+        // empty
+    };
+
+    void queue(void*, size_t);
+    size_t read_back(size_t);
+    void reset(size_t);
+    void reset();
+
+    std::shared_ptr<BYTE> out_buf_;
+
+private:
+    std::mutex buffer_mutex_;
+    std::unique_ptr<BYTE> buf_;
+    size_t max_size_;
+    size_t head_ = 0;
+    size_t lookback_ = 0;
+};
