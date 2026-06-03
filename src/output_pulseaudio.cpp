eaa9cd9b1b59d2bafa45628e66e8c4426592a6b8
jiwon821 <251018584+jiwon821@users.noreply.github.com>
2026-06-03 20:31:05 +0300

    Sorting and formatting

--- src/output_pulseaudio.cpp
+++ src/output_pulseaudio.cpp
@@ -864,137 +864,72 @@ bool output_pulse::load_pulse_dll()
         return false;
     }
 
-    g_pa_strerror = (pa_strerror)GetProcAddress(libpulse, "pa_strerror");
-    g_pa_threaded_mainloop_new = (pa_threaded_mainloop_new)GetProcAddress(libpulse, "pa_threaded_mainloop_new");
-    g_pa_threaded_mainloop_free = (pa_threaded_mainloop_free)GetProcAddress(libpulse, "pa_threaded_mainloop_free");
-    g_pa_threaded_mainloop_start = (pa_threaded_mainloop_start)GetProcAddress(libpulse, "pa_threaded_mainloop_start");
-    g_pa_threaded_mainloop_stop = (pa_threaded_mainloop_stop)GetProcAddress(libpulse, "pa_threaded_mainloop_stop");
-    g_pa_threaded_mainloop_lock = (pa_threaded_mainloop_lock)GetProcAddress(libpulse, "pa_threaded_mainloop_lock");
-    g_pa_threaded_mainloop_unlock = (pa_threaded_mainloop_unlock)GetProcAddress(libpulse, "pa_threaded_mainloop_unlock");
-    g_pa_threaded_mainloop_wait = (pa_threaded_mainloop_wait)GetProcAddress(libpulse, "pa_threaded_mainloop_wait");
-    g_pa_threaded_mainloop_signal = (pa_threaded_mainloop_signal)GetProcAddress(libpulse, "pa_threaded_mainloop_signal");
-    g_pa_threaded_mainloop_accept = (pa_threaded_mainloop_accept)GetProcAddress(libpulse, "pa_threaded_mainloop_accept");
-    g_pa_threaded_mainloop_get_retval = (pa_threaded_mainloop_get_retval)GetProcAddress(libpulse, "pa_threaded_mainloop_get_retval");
-    g_pa_threaded_mainloop_get_api = (pa_threaded_mainloop_get_api)GetProcAddress(libpulse, "pa_threaded_mainloop_get_api");
-    g_pa_stream_new = (pa_stream_new)GetProcAddress(libpulse, "pa_stream_new");
-    g_pa_stream_connect_playback = (pa_stream_connect_playback)GetProcAddress(libpulse, "pa_stream_connect_playback");
-    g_pa_stream_disconnect = (pa_stream_disconnect)GetProcAddress(libpulse, "pa_stream_disconnect");
-    g_pa_stream_unref = (pa_stream_unref)GetProcAddress(libpulse, "pa_stream_unref");
-    g_pa_stream_write = (pa_stream_write)GetProcAddress(libpulse, "pa_stream_write");
-    g_pa_stream_cancel_write = (pa_stream_cancel_write)GetProcAddress(libpulse, "pa_stream_cancel_write");
-    g_pa_stream_drop = (pa_stream_drop)GetProcAddress(libpulse, "pa_stream_drop");
-    g_pa_stream_writable_size = (pa_stream_writable_size)GetProcAddress(libpulse, "pa_stream_writable_size");
-    g_pa_stream_drain = (pa_stream_drain)GetProcAddress(libpulse, "pa_stream_drain");
-    g_pa_stream_set_write_callback = (pa_stream_set_write_callback)GetProcAddress(libpulse, "pa_stream_set_write_callback");
-    g_pa_stream_set_state_callback = (pa_stream_set_state_callback)GetProcAddress(libpulse, "pa_stream_set_state_callback");
-    g_pa_stream_set_started_callback = (pa_stream_set_started_callback)GetProcAddress(libpulse, "pa_stream_set_started_callback");
-    g_pa_stream_set_underflow_callback = (pa_stream_set_underflow_callback)GetProcAddress(libpulse, "pa_stream_set_underflow_callback");
-    g_pa_stream_cork = (pa_stream_cork)GetProcAddress(libpulse, "pa_stream_cork");
-    g_pa_stream_is_corked = (pa_stream_is_corked)GetProcAddress(libpulse, "pa_stream_is_corked");
-    g_pa_stream_flush = (pa_stream_flush)GetProcAddress(libpulse, "pa_stream_flush");
-    g_pa_stream_update_sample_rate = (pa_stream_update_sample_rate)GetProcAddress(libpulse, "pa_stream_update_sample_rate");
-    g_pa_stream_get_state = (pa_stream_get_state)GetProcAddress(libpulse, "pa_stream_get_state");
-    g_pa_stream_get_sample_spec = (pa_stream_get_sample_spec)GetProcAddress(libpulse, "pa_stream_get_sample_spec");
-    g_pa_stream_get_latency = (pa_stream_get_latency)GetProcAddress(libpulse, "pa_stream_get_latency");
-    g_pa_stream_get_timing_info = (pa_stream_get_timing_info)GetProcAddress(libpulse, "pa_stream_get_timing_info");
-    g_pa_stream_trigger = (pa_stream_trigger)GetProcAddress(libpulse, "pa_stream_trigger");
-    g_pa_stream_update_timing_info = (pa_stream_update_timing_info)GetProcAddress(libpulse, "pa_stream_update_timing_info");
-    g_pa_stream_get_buffer_attr = (pa_stream_get_buffer_attr)GetProcAddress(libpulse, "pa_stream_get_buffer_attr");
-    g_pa_proplist_new = (pa_proplist_new)GetProcAddress(libpulse, "pa_proplist_new");
-    g_pa_proplist_free = (pa_proplist_free)GetProcAddress(libpulse, "pa_proplist_free");
-    g_pa_proplist_set = (pa_proplist_set)GetProcAddress(libpulse, "pa_proplist_set");
-    g_pa_proplist_sets = (pa_proplist_sets)GetProcAddress(libpulse, "pa_proplist_sets");
-    g_pa_proplist_setp = (pa_proplist_setp)GetProcAddress(libpulse, "pa_proplist_setp");
-    g_pa_context_new_with_proplist = (pa_context_new_with_proplist)GetProcAddress(libpulse, "pa_context_new_with_proplist");
-    g_pa_context_unref = (pa_context_unref)GetProcAddress(libpulse, "pa_context_unref");
-    g_pa_context_errno = (pa_context_errno)GetProcAddress(libpulse, "pa_context_errno");
-    g_pa_context_connect = (pa_context_connect)GetProcAddress(libpulse, "pa_context_connect");
-    g_pa_context_disconnect = (pa_context_disconnect)GetProcAddress(libpulse, "pa_context_disconnect");
-    g_pa_context_get_state = (pa_context_get_state)GetProcAddress(libpulse, "pa_context_get_state");
-    g_pa_context_set_state_callback = (pa_context_set_state_callback)GetProcAddress(libpulse, "pa_context_set_state_callback");
-    g_pa_context_set_event_callback = (pa_context_set_event_callback)GetProcAddress(libpulse, "pa_context_set_event_callback");
-    g_pa_channel_map_init_auto = (pa_channel_map_init_auto)GetProcAddress(libpulse, "pa_channel_map_init_auto");
-    g_pa_operation_unref = (pa_operation_unref)GetProcAddress(libpulse, "pa_operation_unref");
-    g_pa_operation_get_state = (pa_operation_get_state)GetProcAddress(libpulse, "pa_operation_get_state");
-    g_pa_bytes_to_usec = (pa_bytes_to_usec)GetProcAddress(libpulse, "pa_bytes_to_usec");
-    g_pa_usec_to_bytes = (pa_usec_to_bytes)GetProcAddress(libpulse, "pa_usec_to_bytes");
-    g_pa_stream_get_index = (pa_stream_get_index)GetProcAddress(libpulse, "pa_stream_get_index");
-    g_pa_sw_volume_from_dB = (pa_sw_volume_from_dB)GetProcAddress(libpulse, "pa_sw_volume_from_dB");
-    g_pa_sw_volume_to_dB = (pa_sw_volume_to_dB)GetProcAddress(libpulse, "pa_sw_volume_to_dB");
-    g_pa_context_set_sink_input_volume = (pa_context_set_sink_input_volume)GetProcAddress(libpulse, "pa_context_set_sink_input_volume");
-    g_pa_cvolume_init = (pa_cvolume_init)GetProcAddress(libpulse, "pa_cvolume_init");
-    g_pa_cvolume_set = (pa_cvolume_set)GetProcAddress(libpulse, "pa_cvolume_set");
-    g_pa_cvolume_valid = (pa_cvolume_valid)GetProcAddress(libpulse, "pa_cvolume_valid");
-    g_pa_cvolume_equal = (pa_cvolume_equal)GetProcAddress(libpulse, "pa_cvolume_equal");
-    g_pa_context_get_sink_input_info = (pa_context_get_sink_input_info)GetProcAddress(libpulse, "pa_context_get_sink_input_info");
-    g_pa_context_subscribe = (pa_context_subscribe)GetProcAddress(libpulse, "pa_context_subscribe");
-    g_pa_context_set_subscribe_callback = (pa_context_set_subscribe_callback)GetProcAddress(libpulse, "pa_context_set_subscribe_callback");
-
-    if (g_pa_strerror == NULL ||
-        g_pa_threaded_mainloop_new == NULL ||
-        g_pa_threaded_mainloop_free == NULL ||
-        g_pa_threaded_mainloop_start == NULL ||
-        g_pa_threaded_mainloop_stop == NULL ||
-        g_pa_threaded_mainloop_lock == NULL ||
-        g_pa_threaded_mainloop_unlock == NULL ||
-        g_pa_threaded_mainloop_wait == NULL ||
-        g_pa_threaded_mainloop_signal == NULL ||
-        g_pa_threaded_mainloop_accept == NULL ||
-        g_pa_threaded_mainloop_get_retval == NULL ||
-        g_pa_threaded_mainloop_get_api == NULL ||
-        g_pa_stream_new == NULL ||
-        g_pa_stream_connect_playback == NULL ||
-        g_pa_stream_disconnect == NULL ||
-        g_pa_stream_unref == NULL ||
-        g_pa_stream_write == NULL ||
-        g_pa_stream_cancel_write == NULL ||
-        g_pa_stream_drop == NULL ||
-        g_pa_stream_writable_size == NULL ||
-        g_pa_stream_drain == NULL ||
-        g_pa_stream_set_write_callback == NULL ||
-        g_pa_stream_set_state_callback == NULL ||
-        g_pa_stream_set_started_callback == NULL ||
-        g_pa_stream_set_underflow_callback == NULL ||
-        g_pa_stream_cork == NULL ||
-        g_pa_stream_is_corked == NULL ||
-        g_pa_stream_flush == NULL ||
-        g_pa_stream_update_sample_rate == NULL ||
-        g_pa_stream_get_state == NULL ||
-        g_pa_stream_get_sample_spec == NULL ||
-        g_pa_stream_get_latency == NULL ||
-        g_pa_stream_get_timing_info == NULL ||
-        g_pa_stream_trigger == NULL ||
-        g_pa_stream_update_timing_info == NULL ||
-        g_pa_stream_get_buffer_attr == NULL ||
-        g_pa_proplist_new == NULL ||
-        g_pa_proplist_free == NULL ||
-        g_pa_proplist_set == NULL ||
-        g_pa_proplist_sets == NULL ||
-        g_pa_proplist_setp == NULL ||
-        g_pa_context_new_with_proplist == NULL ||
-        g_pa_context_unref == NULL ||
-        g_pa_context_errno == NULL ||
-        g_pa_context_connect == NULL ||
-        g_pa_context_disconnect == NULL ||
-        g_pa_context_get_state == NULL ||
-        g_pa_context_set_state_callback == NULL ||
-        g_pa_context_set_event_callback == NULL ||
-        g_pa_channel_map_init_auto == NULL ||
-        g_pa_operation_unref == NULL ||
-        g_pa_operation_get_state == NULL ||
-        g_pa_bytes_to_usec == NULL ||
-        g_pa_stream_get_index == NULL ||
-        g_pa_sw_volume_from_dB == NULL ||
-        g_pa_sw_volume_to_dB == NULL ||
-        g_pa_context_set_sink_input_volume == NULL ||
-        g_pa_cvolume_init == NULL ||
-        g_pa_cvolume_set == NULL ||
-        g_pa_cvolume_valid == NULL ||
-        g_pa_cvolume_equal == NULL ||
-        g_pa_context_get_sink_input_info == NULL ||
-        g_pa_context_subscribe == NULL ||
-        g_pa_context_set_subscribe_callback == NULL ||
-        g_pa_usec_to_bytes == NULL)
+    // just assign and compare the value with null at the same if, since this is so horrible no matter how we do this
+    if (!(g_pa_bytes_to_usec                    = (pa_bytes_to_usec)GetProcAddress(libpulse,                    "pa_bytes_to_usec")) ||
+        !(g_pa_channel_map_init_auto            = (pa_channel_map_init_auto)GetProcAddress(libpulse,            "pa_channel_map_init_auto")) ||
+        !(g_pa_context_connect                  = (pa_context_connect)GetProcAddress(libpulse,                  "pa_context_connect")) ||
+        !(g_pa_context_disconnect               = (pa_context_disconnect)GetProcAddress(libpulse,               "pa_context_disconnect")) ||
+        !(g_pa_context_errno                    = (pa_context_errno)GetProcAddress(libpulse,                    "pa_context_errno")) ||
+        !(g_pa_context_get_sink_input_info      = (pa_context_get_sink_input_info)GetProcAddress(libpulse,      "pa_context_get_sink_input_info")) ||
+        !(g_pa_context_get_state                = (pa_context_get_state)GetProcAddress(libpulse,                "pa_context_get_state")) ||
+        !(g_pa_context_new_with_proplist        = (pa_context_new_with_proplist)GetProcAddress(libpulse,        "pa_context_new_with_proplist")) ||
+        !(g_pa_context_set_event_callback       = (pa_context_set_event_callback)GetProcAddress(libpulse,       "pa_context_set_event_callback")) ||
+        !(g_pa_context_set_sink_input_volume    = (pa_context_set_sink_input_volume)GetProcAddress(libpulse,    "pa_context_set_sink_input_volume")) ||
+        !(g_pa_context_set_state_callback       = (pa_context_set_state_callback)GetProcAddress(libpulse,       "pa_context_set_state_callback")) ||
+        !(g_pa_context_set_subscribe_callback   = (pa_context_set_subscribe_callback)GetProcAddress(libpulse,   "pa_context_set_subscribe_callback")) ||
+        !(g_pa_context_subscribe                = (pa_context_subscribe)GetProcAddress(libpulse,                "pa_context_subscribe")) ||
+        !(g_pa_context_unref                    = (pa_context_unref)GetProcAddress(libpulse,                    "pa_context_unref")) ||
+        !(g_pa_cvolume_equal                    = (pa_cvolume_equal)GetProcAddress(libpulse,                    "pa_cvolume_equal")) ||
+        !(g_pa_cvolume_init                     = (pa_cvolume_init)GetProcAddress(libpulse,                     "pa_cvolume_init")) ||
+        !(g_pa_cvolume_set                      = (pa_cvolume_set)GetProcAddress(libpulse,                      "pa_cvolume_set")) ||
+        !(g_pa_cvolume_valid                    = (pa_cvolume_valid)GetProcAddress(libpulse,                    "pa_cvolume_valid")) ||
+        !(g_pa_operation_get_state              = (pa_operation_get_state)GetProcAddress(libpulse,              "pa_operation_get_state")) ||
+        !(g_pa_operation_unref                  = (pa_operation_unref)GetProcAddress(libpulse,                  "pa_operation_unref")) ||
+        !(g_pa_proplist_free                    = (pa_proplist_free)GetProcAddress(libpulse,                    "pa_proplist_free")) ||
+        !(g_pa_proplist_new                     = (pa_proplist_new)GetProcAddress(libpulse,                     "pa_proplist_new")) ||
+        !(g_pa_proplist_set                     = (pa_proplist_set)GetProcAddress(libpulse,                     "pa_proplist_set")) ||
+        !(g_pa_proplist_setp                    = (pa_proplist_setp)GetProcAddress(libpulse,                    "pa_proplist_setp")) ||
+        !(g_pa_proplist_sets                    = (pa_proplist_sets)GetProcAddress(libpulse,                    "pa_proplist_sets")) ||
+        !(g_pa_stream_cancel_write              = (pa_stream_cancel_write)GetProcAddress(libpulse,              "pa_stream_cancel_write")) ||
+        !(g_pa_stream_connect_playback          = (pa_stream_connect_playback)GetProcAddress(libpulse,          "pa_stream_connect_playback")) ||
+        !(g_pa_stream_cork                      = (pa_stream_cork)GetProcAddress(libpulse,                      "pa_stream_cork")) ||
+        !(g_pa_stream_disconnect                = (pa_stream_disconnect)GetProcAddress(libpulse,                "pa_stream_disconnect")) ||
+        !(g_pa_stream_drain                     = (pa_stream_drain)GetProcAddress(libpulse,                     "pa_stream_drain")) ||
+        !(g_pa_stream_drop                      = (pa_stream_drop)GetProcAddress(libpulse,                      "pa_stream_drop")) ||
+        !(g_pa_stream_flush                     = (pa_stream_flush)GetProcAddress(libpulse,                     "pa_stream_flush")) ||
+        !(g_pa_stream_get_buffer_attr           = (pa_stream_get_buffer_attr)GetProcAddress(libpulse,           "pa_stream_get_buffer_attr")) ||
+        !(g_pa_stream_get_index                 = (pa_stream_get_index)GetProcAddress(libpulse,                 "pa_stream_get_index")) ||
+        !(g_pa_stream_get_latency               = (pa_stream_get_latency)GetProcAddress(libpulse,               "pa_stream_get_latency")) ||
+        !(g_pa_stream_get_sample_spec           = (pa_stream_get_sample_spec)GetProcAddress(libpulse,           "pa_stream_get_sample_spec")) ||
+        !(g_pa_stream_get_state                 = (pa_stream_get_state)GetProcAddress(libpulse,                 "pa_stream_get_state")) ||
+        !(g_pa_stream_get_timing_info           = (pa_stream_get_timing_info)GetProcAddress(libpulse,           "pa_stream_get_timing_info")) ||
+        !(g_pa_stream_is_corked                 = (pa_stream_is_corked)GetProcAddress(libpulse,                 "pa_stream_is_corked")) ||
+        !(g_pa_stream_new                       = (pa_stream_new)GetProcAddress(libpulse,                       "pa_stream_new")) ||
+        !(g_pa_stream_set_started_callback      = (pa_stream_set_started_callback)GetProcAddress(libpulse,      "pa_stream_set_started_callback")) ||
+        !(g_pa_stream_set_state_callback        = (pa_stream_set_state_callback)GetProcAddress(libpulse,        "pa_stream_set_state_callback")) ||
+        !(g_pa_stream_set_underflow_callback    = (pa_stream_set_underflow_callback)GetProcAddress(libpulse,    "pa_stream_set_underflow_callback")) ||
+        !(g_pa_stream_set_write_callback        = (pa_stream_set_write_callback)GetProcAddress(libpulse,        "pa_stream_set_write_callback")) ||
+        !(g_pa_stream_trigger                   = (pa_stream_trigger)GetProcAddress(libpulse,                   "pa_stream_trigger")) ||
+        !(g_pa_stream_unref                     = (pa_stream_unref)GetProcAddress(libpulse,                     "pa_stream_unref")) ||
+        !(g_pa_stream_update_sample_rate        = (pa_stream_update_sample_rate)GetProcAddress(libpulse,        "pa_stream_update_sample_rate")) ||
+        !(g_pa_stream_update_timing_info        = (pa_stream_update_timing_info)GetProcAddress(libpulse,        "pa_stream_update_timing_info")) ||
+        !(g_pa_stream_writable_size             = (pa_stream_writable_size)GetProcAddress(libpulse,             "pa_stream_writable_size")) ||
+        !(g_pa_stream_write                     = (pa_stream_write)GetProcAddress(libpulse,                     "pa_stream_write")) ||
+        !(g_pa_strerror                         = (pa_strerror)GetProcAddress(libpulse,                         "pa_strerror")) ||
+        !(g_pa_sw_volume_from_dB                = (pa_sw_volume_from_dB)GetProcAddress(libpulse,                "pa_sw_volume_from_dB")) ||
+        !(g_pa_sw_volume_to_dB                  = (pa_sw_volume_to_dB)GetProcAddress(libpulse,                  "pa_sw_volume_to_dB")) ||
+        !(g_pa_threaded_mainloop_accept         = (pa_threaded_mainloop_accept)GetProcAddress(libpulse,         "pa_threaded_mainloop_accept")) ||
+        !(g_pa_threaded_mainloop_free           = (pa_threaded_mainloop_free)GetProcAddress(libpulse,           "pa_threaded_mainloop_free")) ||
+        !(g_pa_threaded_mainloop_get_api        = (pa_threaded_mainloop_get_api)GetProcAddress(libpulse,        "pa_threaded_mainloop_get_api")) ||
+        !(g_pa_threaded_mainloop_get_retval     = (pa_threaded_mainloop_get_retval)GetProcAddress(libpulse,     "pa_threaded_mainloop_get_retval")) ||
+        !(g_pa_threaded_mainloop_lock           = (pa_threaded_mainloop_lock)GetProcAddress(libpulse,           "pa_threaded_mainloop_lock")) ||
+        !(g_pa_threaded_mainloop_new            = (pa_threaded_mainloop_new)GetProcAddress(libpulse,            "pa_threaded_mainloop_new")) ||
+        !(g_pa_threaded_mainloop_signal         = (pa_threaded_mainloop_signal)GetProcAddress(libpulse,         "pa_threaded_mainloop_signal")) ||
+        !(g_pa_threaded_mainloop_start          = (pa_threaded_mainloop_start)GetProcAddress(libpulse,          "pa_threaded_mainloop_start")) ||
+        !(g_pa_threaded_mainloop_stop           = (pa_threaded_mainloop_stop)GetProcAddress(libpulse,           "pa_threaded_mainloop_stop")) ||
+        !(g_pa_threaded_mainloop_unlock         = (pa_threaded_mainloop_unlock)GetProcAddress(libpulse,         "pa_threaded_mainloop_unlock")) ||
+        !(g_pa_threaded_mainloop_wait           = (pa_threaded_mainloop_wait)GetProcAddress(libpulse,           "pa_threaded_mainloop_wait")) ||
+        !(g_pa_usec_to_bytes                    = (pa_usec_to_bytes)GetProcAddress(libpulse,                    "pa_usec_to_bytes")))
     {
         console::error("Error loading external functions from libpulse-0.dll");
         return false;
